8bdadf9119e7d29fec4c4aaf73984e628135ead7
AminnimAMoh <43339115+AminnimAMoh@users.noreply.github.com>
2021-07-31 04:02:44 +1000

    Straigts checked.
    I trust mu alguritm.

--- src/CheckForStraight.ts
+++ src/CheckForStraight.ts
@@ -9,28 +9,46 @@ export const checkForStraight = (
   //Receiving players Pair status to check if they have pairs that are not eligible for this section.
   { playerOnePairStatus, playerTwoPairStatus }: PairState
 ) => {
+
+  //Two arrays for reserving the sorted arrays
   let playerOne: (string | number | null)[] = [null];
   let playerTwo: (string | number | null)[] = [null];
 
-  if (playerOnePairStatus === "none") playerOne = playerOneValues.sort((a: any,b: any)=> {return a - b});
-  if (playerTwoPairStatus === "none") playerTwo = playerTwoValues.sort((a: any,b: any)=> {return a - b});
+  //Sorting hands
+  playerOne = playerOneValues.sort((a: any,b: any)=> {return a - b});
+  playerTwo = playerTwoValues.sort((a: any,b: any)=> {return a - b});
 
+  //Cheking for Consecutive hands
   let playerOneStraightState=false; 
+  let playerTwoStraightState=false; 
+  //The iterations showed there is no Consecutive hand
   if (playerOnePairStatus === "none") playerOneStraightState = checkForConsecutive(playerOne);
-  playerOneStraightState && console.log(playerOneStraightState,playerOneValues);
+  if (playerOnePairStatus === "none") playerTwoStraightState = checkForConsecutive(playerTwo);
 };
 
 const checkForConsecutive = (data: any): boolean => {
-  if (data !== null) {
+  //An index to stop the loop at the end of the data array.
     let i: number = 0;
+    //d is a variable to save the distance.
     let d = null;
+    //A visited boolean to turn false if visited.
+    //False would mean the hand is not Consecutive
     let check = true;
-    while (i < data.length) {
+
+    //Breaking out of the loop if index reached the end of the data or the visited variable is false.
+    //Breaking the loop helps time complexity a littel as cuting out unnessesary itterations.
+    while (i < data.length || !check) {
+      //Calculating the distance between two close indexes
       d = data[i + 1] - data[i];
+      //if the distance between two numbers is not equal one 
+      //that means the hand is not straight.
+      //As we have sorted the array we can trust naighbers. 
       if (d !== 1) check = false;
+
+      //Increasing the index
       i++;
     }
+
+    //Returning the visiting variable.
     return check;
-  }
-  return false
 };
