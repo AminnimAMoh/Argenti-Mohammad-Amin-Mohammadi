ed51c4a038eb20a527124d88adaf4a2fd9fb59fc
AminnimAMoh <43339115+AminnimAMoh@users.noreply.github.com>
2021-07-30 15:33:10 +1000

    Start Working.
    Reading data and sorting the data in an object for two players.

---
+++ App/App.js
@@ -0,0 +1,18 @@
+const fs = require("fs");
+
+let inputData = [];
+
+fs.readFile("Files/poker-hands.txt", "utf8", (err, res) => {
+  err && console.log(err);
+  inputData = res.split("\n");
+  let hands = {
+    playerOne: [],
+    playerTwo: [],
+  };
+  inputData.map((data) => {
+    const splitedData=data.split(" ");
+    console.log(splitedData);
+  });
+
+  console.log(inputData);
+});
