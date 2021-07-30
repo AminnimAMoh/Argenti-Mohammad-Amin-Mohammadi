2127dbb67afe24f0f637a89d0c1d967e0b2454d5
AminnimAMoh <43339115+AminnimAMoh@users.noreply.github.com>
2021-07-30 16:07:52 +1000

    Reading data.
    Players hands are suffeled and reserved in an object containing to arrays.
    
    Each array contains hands for one player.

--- App/App.js
+++ App/App.js
@@ -9,10 +9,16 @@ fs.readFile("Files/poker-hands.txt", "utf8", (err, res) => {
     playerOne: [],
     playerTwo: [],
   };
-  inputData.map((data) => {
+  inputData.map((data, dataIndex) => {
     const splitedData=data.split(" ");
-    console.log(splitedData);
+    let handOne=[] 
+    let handTwo=[]
+    splitedData.forEach((handData,index)=>{
+        index<5 ? handOne.push(handData) : handTwo.push(handData)
+        index===9 && hands.playerOne.push(handOne); hands.playerTwo.push(handTwo)
+    })
   });
+  console.log(hands);
 
 //   console.log(inputData);
 });
