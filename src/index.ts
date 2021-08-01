04400c50c121d4d048b2f10e4a718d4aeffe7df6
AminnimAMoh <43339115+AminnimAMoh@users.noreply.github.com>
2021-08-01 16:11:11 +1000

    All Done!!!.
    You can run the program in command prompt/Terminal by runing yarn dev.

--- src/index.ts
+++ src/index.ts
@@ -133,6 +133,6 @@ fs.readFile("Files/poker-hands.txt", "utf8", (err: string, res: string) => {
 
   //Creating the STDOUT.text file containing the programm results.
   fs.writeFile('STDOUT.text', `Player One :  ${playerOneWins} Hands \n Player Two :  ${playerTwoWins} Hands`, (err: any)=>{
-    err ? console.log(err) : console.log('STDOUT Ready to read. Thank you for your time.');
+    err ? console.log(err) : console.log('STDOUT Ready to read. You can find this file in root folder of the project. Thank you for your time.');
   })
 });
